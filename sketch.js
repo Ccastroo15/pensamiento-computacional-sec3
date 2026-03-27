3aaf275427ee4b6cc339454a817ec6d630d191a8
Ccastroo15 <claudia.castro3@mail.udp.cl>
2026-03-27 12:17:54 -0300

    Add files via upload
    primer ejercicio formas básicas

---
+++ sketch.js
@@ -0,0 +1,20 @@
+function setup() {
+  createCanvas(400, 600);
+}
+
+function draw() {
+  background(255);
+  
+  fill(250,222,37)
+  ellipse(200,200,150,150);
+  
+  fill(0)
+  ellipse(170,180,20,20);
+  ellipse(240,180,20,20);
+  
+  strokeWeight(5);
+  stroke(50);
+  line(230,220,200,220);
+
+  
+}
